7d7723215171b05c0981b9803c6b324c339e58a9
am <am@MacBook-Pro.local>
2019-01-23 13:52:57 -0800

    added photos to presidential primary candidates

--- src/app/component/presidential-primaries/presidential-primaries.component.ts
+++ src/app/component/presidential-primaries/presidential-primaries.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UrlService } from 'src/app/repService/url.service';
 
 @Component({
   selector: 'app-presidential-primaries',
@@ -8,23 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class PresidentialPrimariesComponent implements OnInit {
 
   // hardwiring these here. can use Rep -- a bit more work, but more robust.
-  warrenId = '2-S366';
-  gillibrandId = '2-S331';
-  harrisId = '2-S387';
 
-  // house
-  gabbardId = '1-G000571';
-  castroId = '1-C001091';
-  swalwellId = '1-S001193';
+  warren = { chamberNameId: '2-S366', bioguide: 'W000817' };
+  gillibrand = { chamberNameId: '2-S331', bioguide: 'G000555' };
+  harris = { chamberNameId: '2-S387', bioguide: 'H001075' };
+
+  gabbard = { chamberNameId: '1-G000571', bioguide: 'G000571' };
+  castro = { chamberNameId: '1-C001091', bioguide: 'C001091' };
+  swalwell = { chamberNameId: '1-S001193', bioguide: 'S001193' };
 
 
-  constructor() { }
+  constructor(public urlService: UrlService) { }
 
   ngOnInit() {
   }
 
   csv(id1, id2) {
     return id1 + "," + id2;
+  }
+
+  public getPhotoUrl(rep) {
+
+    const repPhotoUrl = this.urlService.getRepPhotoUrl(rep.bioguide);
+    return repPhotoUrl;
+
   }
 
 }
