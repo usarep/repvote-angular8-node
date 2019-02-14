922382129c3a9243607e6949c0424b07ddd8675a
am <am@MacBook-Pro.local>
2019-02-14 10:08:20 -0800

    added test files to backend

--- backend/app.js
+++ backend/app.js
@@ -57,6 +57,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/photo", express.static(path.join(__dirname, "/repPhoto")));
 app.use("/", express.static(path.join(__dirname, "/angular")));
+app.use("/iframeTest", express.static(path.join(__dirname, "/iframeTest")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
