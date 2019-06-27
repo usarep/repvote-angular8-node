cb509d8819b9eff0c43e9fdc2dcebca1e4fe3619
am <am@MacBook-Pro.local>
2019-06-27 14:29:55 -0700

    bug fix

--- src/app/component/presidential-primaries/presidential-primaries-succinct/presidential-primaries-succinct.component.ts
+++ src/app/component/presidential-primaries/presidential-primaries-succinct/presidential-primaries-succinct.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PresidentialPrimariesV2Component } from '../presidential-primaries-v2/presidential-primaries-v2.component';
 import { UrlService } from 'src/app/repService/url.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-presidential-primaries-succinct',
@@ -9,25 +10,32 @@ import { UrlService } from 'src/app/repService/url.service';
 })
 export class PresidentialPrimariesSuccinctComponent extends PresidentialPrimariesV2Component implements OnInit {
 
-  constructor(public urlService: UrlService) {
+  constructor(public urlService: UrlService, private _router: Router) {
     super(urlService);
    }
 
   ngOnInit() {
-    this.houseMembers = [this.delaney,
+    // if this is not an iframe, redirect to /presidentialPrimaries
+    if (window.self === window.top) {
+      this._router.navigate(["/presidentialPrimaries"]);
+    }
+    else {
+      // this is an iFrame
+      this.houseMembers = [this.delaney,
       this.gabbard,
       this.moulton,
       this.orourke,
       this.ryanOH,
       this.swalwell];
 
-    this.senators = [this.bennet,
+      this.senators = [this.bennet,
       this.booker,
       this.gillibrand,
       this.harris,
       this.klobuchar,
       this.sanders,
       this.warren];
+    }
   }
 
 }
