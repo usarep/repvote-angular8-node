215865b0925d8074752a642b07012af62928df3c
am <am@MacBook-Pro.local>
2019-09-01 17:38:26 -0700

    migration to usarep.org

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   // server: "http://localhost:8080", // this is the java server
-  server: "https://api.compararep.com",
-  imgServer: "https://www.compararep.com", // serves rep photos
+  server: "https://api.usarep.org",
+  imgServer: "https://www.usarep.org", // serves rep photos
 
   // client side key for reCaptcha V3. supply your own
   reCaptchaV3ClientKey: '6LfzjGoUAAAAAI3AQKhMdItGds9RbSKlK5gEqrex',
